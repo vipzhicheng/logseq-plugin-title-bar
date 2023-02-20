38c201c418269c36fa72fcc8e4971ab5ebb203fb
Richard Yu <vipzhicheng@gmail.com>
2023-02-20 10:43:40 +0800

    add title color settings

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import "@logseq/libs";
+import { SettingSchemaDesc } from "@logseq/libs/dist/LSPlugin";
 import "./style.css";
 
 function createModel() {
@@ -13,19 +14,58 @@ async function triggerBlockModal() {
   createModel().openModal();
 }
 
+const defineSettings: SettingSchemaDesc[] = [
+  {
+    key: "titleColor",
+    title: "Title Color",
+    description: "Set title color",
+    type: "string",
+    inputAs: "color",
+    default: "#ff0000",
+  },
+];
+
+logseq.useSettingsSchema(defineSettings);
+
+const setTitle = async () => {
+  const config = await logseq.App.getCurrentGraph();
+  logseq.provideUI({
+    template: `<a data-on-click="openMyToySettings" title="My Toy Settings">${config?.name}</a> <a class="cursor-pointer" data-on-click="openPluginSettings" style="display: inline-block;" title="Plugin Settings">
+    <i class="ti ti-settings" style=""></i>
+    </a>`,
+    path: "#logseq-title",
+    // reset: true,
+    replace: true,
+    key: "logseq-my-toy",
+  });
+
+  logseq.provideStyle({
+    key: "logseq-my-toy-title",
+    style: `
+
+#logseq-title {
+  padding: 2rem;
+  font-size: 1rem;
+  font-weight: bold;
+  flex: 1;
+}
+
+#logseq-title a {
+  color: ${logseq.settings?.titleColor || "#ff0000"};
+}
+    `,
+  });
+};
+
 const main = async () => {
   logseq.provideModel({
     openPluginSettings() {
       logseq.App.invokeExternalCommand("logseq.ui/toggle-settings");
     },
+    openMyToySettings() {
+      logseq.showSettingsUI();
+    },
   });
-  const config = await logseq.App.getCurrentGraph();
-  // logseq.App.registerUIItem("toolbar", {
-  //   key: "logseq-my-toy",
-  //   template: `
-  //    <strong>Hello</strong>
-  //   `,
-  // });
 
   const container = top?.document.querySelector(
     ".cp__header>.r"
@@ -34,43 +74,14 @@ const main = async () => {
   titleEl.id = "logseq-title";
   container.insertAdjacentElement("afterbegin", titleEl);
 
-  logseq.provideUI({
-    template: `${config?.name} <a class="cursor-pointer" data-on-click="openPluginSettings" style="display: inline-block;" title="Plugin Settings">
-    <i class="ti ti-settings" style=""></i>
-    </a>`,
-    path: "#logseq-title",
-    replace: true,
-    key: "logseq-my-toy",
-  });
-
-  logseq.provideStyle({
-    key: "logseq-my-toy",
-    style: `
-
-#logseq-title {
-  padding: 2rem;
-  font-size: 1rem;
-  font-weight: bold;
-  color: darkviolet;
-  flex: 1;
-}
-
-
-
-    `,
-  });
+  await setTitle();
 
   logseq.App.onCurrentGraphChanged(async () => {
-    const config = await logseq.App.getCurrentGraph();
-    logseq.provideUI({
-      template: `${config?.name} <a class="cursor-pointer" data-on-click="openPluginSettings" style="display: inline-block;" title="Plugin Settings">
-      <i class="ti ti-settings" style=""></i>
-      </a>`,
-      path: "#logseq-title",
-      // reset: true,
-      replace: true,
-      key: "logseq-my-toy",
-    });
+    await setTitle();
+  });
+
+  logseq.onSettingsChanged(async () => {
+    await setTitle();
   });
 };
 
